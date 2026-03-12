074f10a69f26bf4a294be8b6d9434539ff408182
Phil Gee <originalphil@gmail.com>
2026-03-12 14:43:11 +0000

    Remove unused constant.

--- lib/definitions/constants.ts
+++ lib/definitions/constants.ts
@@ -1,4 +1,3 @@
 export const OFFICIAL_PYPI_REGISTRY = 'https://pypi.org/';
 
 export const GITHUB_ACTIONS_PROVIDER_NAME = 'GitHub Actions';
-export const GITLAB_PIPELINES_PROVIDER_NAME = 'GitLab CI/CD';
